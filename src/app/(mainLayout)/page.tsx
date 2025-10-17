f57d2fa47cbf1939fb8247d1e6ccf00bd30906ba
asir131 <afaysal220@gmail.com>
2025-10-18 03:40:57 +0600

    bug fix

--- src/app/(mainLayout)/page.tsx
+++ src/app/(mainLayout)/page.tsx
@@ -1,14 +1,5 @@
 
-import BackgroundImage from "@/components/Pages/Home/backgroundImage";
-import CarBrandSelector from "@/components/Pages/Home/CarBrandSelector";
-import FAQAccordion from "@/components/Pages/Home/FAQAccordion";
-import FeaturedCars from "@/components/Pages/Home/FeaturedCars";
-import FreeResource from "@/components/Pages/Home/FreeResource";
-import HeroBannerSection from "@/components/Pages/Home/HeroBannerSection";
-import TeamMarketplace from "@/components/Pages/Home/TeamMarketplace";
-import Testimonials from "@/components/Pages/Home/Testimonials";
-import TopRatedUsed from "@/components/Pages/Home/TopRatedUsed";
-import WhyAutowurx from "@/components/Pages/Home/WhyAutowurx";
+
 
 import React from "react";
 import Singup from '@/components/Pages/Vendor/Singup/Singup'
